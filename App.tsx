b41126a1b6ab6ee414074fcf858851434ca3b3ff
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-10-29 01:51:50 -0400

    user created

--- App.tsx
+++ App.tsx
@@ -1,14 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React, {useEffect} from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StreamChat } from "stream-chat";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+
+const API_KEY = "r6bkczhwcgx2";
+const client = StreamChat.getInstance(API_KEY)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+
+useEffect(() => {
+const connectUser = async () => {
+
+  await client.connectUser({
+    id: "vadim",
+    name: "Vadim Savin",
+    image: 'https://i.imgur.com/fR9Jz14.png',
+  },
+  client.devToken('vadim')
+  );
+}
+
+connectUser()
+},[])
 
   if (!isLoadingComplete) {
     return null;
