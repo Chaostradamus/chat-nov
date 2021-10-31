038cbbcba5bd1a7b84eab113ace317386f52995c
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-10-30 23:46:50 -0400

    channel creation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StreamChat } from "stream-chat";
 
@@ -7,29 +7,33 @@ import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
-
 const API_KEY = "r6bkczhwcgx2";
-const client = StreamChat.getInstance(API_KEY)
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+    const connectUser = async () => {
+      await client.connectUser(
+        {
+          id: "vadim",
+          name: "Vadim Savin",
+          image: "https://i.imgur.com/fR9Jz14.png",
+        },
+        client.devToken("vadim")
+      );
+      // channel creation
+      const channel = client.channel("messaging", "notjustdev", {
+        name: "notJust.dev",
+      });
+      await channel.watch()
+    };
 
-useEffect(() => {
-const connectUser = async () => {
-
-  await client.connectUser({
-    id: "vadim",
-    name: "Vadim Savin",
-    image: 'https://i.imgur.com/fR9Jz14.png',
-  },
-  client.devToken('vadim')
-  );
-}
-
-connectUser()
-},[])
+    connectUser();
+    return () => client.disconnectUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
